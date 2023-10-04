e38901eb7ccb0916788eedd0d8aca651954427ed
jc <wnwlcks123@gmail.com>
2023-10-04 14:03:18 +0900

    Feaures 컴포넌트 생성

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,8 +1,58 @@
 import React from 'react'
+import {FaCheck} from 'react-icons/fa'
 
 const Feature = () => {
   return (
-    <div>Feature</div>
+    <section name="feature" className='w-full my-32'>
+      <div className='max-w-[1240px] mx-auto px-2'>
+        <h2 className='text-5xl font-bold text-center'>Features</h2>
+        <p className='py-8 text-2xl text-center text-gray-500'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+        </p>
+
+
+          <div className='grid gap-4 pt-4 sm:grid-cols-2 lg:grid-cols-4'>
+            <div className='flex'>
+              <FaCheck className='mt-1 mr-3 text-blue-600 w-7' />
+              <div>
+                <h3 className='text-lg font-bold'>Real-Time</h3>
+                <p className='pt-2 pb-4 text-lg'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                </p>
+              </div>
+            </div>
+            <div className='flex'>
+              <FaCheck className='mt-1 mr-3 text-blue-600 w-7' />
+              <div>
+                <h3 className='text-lg font-bold'>Real-Time</h3>
+                <p className='pt-2 pb-4 text-lg'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                </p>
+              </div>
+            </div>
+            <div className='flex'>
+              <FaCheck className='mt-1 mr-3 text-blue-600 w-7' />
+              <div>
+                <h3 className='text-lg font-bold'>Real-Time</h3>
+                <p className='pt-2 pb-4 text-lg'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                </p>
+              </div>
+            </div>
+            <div className='flex'>
+              <FaCheck className='mt-1 mr-3 text-blue-600 w-7' />
+              <div>
+                <h3 className='text-lg font-bold'>Real-Time</h3>
+                <p className='pt-2 pb-4 text-lg'>
+                  Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
+                </p>
+              </div>
+            </div>
+        </div>
+      </div>
+        
+
+    </section>
   )
 }
 
