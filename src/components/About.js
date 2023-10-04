0a2f685b3205f2400cbcac4db8b6ec3eb1cd8b8f
jc <wnwlcks123@gmail.com>
2023-10-04 13:07:55 +0900

    About 컴포넌트 생성

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,28 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section 
+      name="about"
+      className='w-full h-screen'
+    >
+      <div className='flex flex-col justify-center w-full h-full max-w-screen-lg p-4 mx-auto'>
+        <div className='text-center'>
+          <h2 className='text-5xl font-bold'>About this company</h2>
+          <p className='py-6 text-3xl text-gray-500'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+
+        </div>
+
+        <p className='mt-20 text-xl'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        </p>
+        <p className='mt-20 text-xl'>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+        </p>
+      </div>
+      About
+    </section>
   )
 }
 
